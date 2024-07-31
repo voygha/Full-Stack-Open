b2b3e806e87ed02c8541be56659486c0dac9d956
voygha <joseluiscastanonalvarado@gmail.com>
2024-07-31 15:31:58 -0600

    feat: Renderizando colecciones

--- part2/renderizando_colecciones/src/App.jsx
+++ part2/renderizando_colecciones/src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(props) {
+  const { notes } = props
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Notes</h1>
+        <ul>
+          <l1>{notes[0].content}</l1>
+          <l1>{notes[1].content}</l1>
+          <l1>{notes[2].content}</l1>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
