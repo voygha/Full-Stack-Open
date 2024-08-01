566bd868f496ea0116fd80b3a5d78fd60cf028b6
voygha <joseluiscastanonalvarado@gmail.com>
2024-08-01 16:06:09 -0600

    feat: Part 2 se creo una funcion para agregar notas

--- part2/renderizando_colecciones/src/App.jsx
+++ part2/renderizando_colecciones/src/App.jsx
@@ -1,8 +1,29 @@
 
 import './App.css'
 import Note from './components/Note'
+import { useState } from 'react'
 
-function App({notes}) {
+function App(props) {
+  const [notes, setNotes] =useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note... ')
+
+  const addNote = (e) =>{
+    e.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id:notes.length + 1
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+    console.log('button Clicked', e.target)
+  }
+
+  const handleNoteChange = (e) =>{
+    console.log(e.target.value)
+    setNewNote(e.target.value)
+  }
+
   return (
     <>
       <div>
@@ -13,6 +34,12 @@ function App({notes}) {
               <Note key={note.id} note={note} />
           )}
         </ul>
+        <form onSubmit={addNote}>
+          <input 
+          value={newNote}
+          onChange={handleNoteChange} />
+          <button type='submit'>Save</button>
+        </form>
       </div>
     </>
   )
